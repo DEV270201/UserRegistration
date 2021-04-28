436d5be525e25fca49f15e17d35826d41ea3b1b6
DEV270201 <devansh.js@somaiya.edu>
2021-04-28 12:23:08 +0530

    setup files

---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config({path : "./config.env"});
+
+const app = require("./app");
+
+const port = process.env.PORT || 3001;
+const server = app.listen(port , ()=>{
+   console.log(`server listening on port ${port}`); 
+});
