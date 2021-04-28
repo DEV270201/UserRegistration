8f0269a849d09e771eaa323333691ee3fa5038e3
DEV270201 <devansh.js@somaiya.edu>
2021-04-28 13:09:03 +0530

    modified

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 
+if(process.env.NODE_ENV === "development"){
+    console.log("in the development stage");
+    console.log(__dirname);
+}
+
 app.use((req,res,next)=>{
     console.log("middleware");
     next();
 });
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 module.exports = app;
