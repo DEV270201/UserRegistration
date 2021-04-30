7161cc2c90a223d548d9e0597804f21e1cf11dad
DEV270201 <devansh.js@somaiya.edu>
2021-04-30 19:20:46 +0530

    modiifed app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/user");
+const {NotFoundError} = require("./Errors");
 
 if(process.env.NODE_ENV === "development"){
     console.log("in the development stage");
-    console.log(__dirname);
 }
 
 app.use((req,res,next)=>{
@@ -12,6 +13,24 @@ app.use((req,res,next)=>{
 });
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 app.use(express.static(`${__dirname}/public`));
+app.use(userRouter);
+
+
+//middleware for the unhandled routes
+app.use("*",(req,res,next)=>{
+   return next(new NotFoundError("Sorry, the requested page is not available"));  //passing the error to the global middleware
+});
+
+//global error middleware
+app.use((err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "Fail";
+    res.status(err.statusCode).json({
+        status : err.status,
+        message : err.message,
+    });
+})
 
 module.exports = app;
