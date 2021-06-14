170a4909a57b4cb6f8cff5c0b7a3efb9746ef0cc
DEV270201 <devansh.js@somaiya.edu>
2021-06-14 11:27:47 +0530

    added passport file for authentication of the user

---
+++ passport.js
@@ -0,0 +1,71 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require("./model/userModel");
+const {AuthenticationError,ClientError} = require("./Errors");
+const bcrypt = require("bcryptjs");
+
+//using the local strategy
+const Authentication = (pass_port) => {
+
+//STEP 1
+    //username and password  should be equal to the name string in login form
+    pass_port.use(new LocalStrategy(async (username,password,done)=>{
+
+    console.log("hello passport");
+    //find the user
+     try{
+         const user = await User.findOne({
+             username : username,
+         })
+
+      if(!user){
+          console.log("user not found");
+          throw new AuthenticationError("User not registered");
+      }
+
+    //match the password
+    const matched = await bcrypt.compare(password , user.password);
+
+    //if the password is incorrect we throw an error
+    if(!matched){
+        throw new ClientError("Please check your password");
+    }
+
+    console.log("user found!!");
+    done(null,user);
+
+     }catch(err){
+         //check the errors
+         if(err.statusCode === 400 || err.statusCode === 401){
+             console.log(err);
+             done(null,false, { message : err.message });
+         }else{
+            console.log("b2");
+            //if the problem is occurred from the server side then we simply pass the user
+            done(err);
+         }
+     }
+}));
+
+//STEP 2
+
+//serializing the user i.e sending some unique property which will be stuffed inside the cookie
+passport.serializeUser((user,done)=>{
+//    done(null,user.id)
+console.log(user);
+console.log(user.id);
+});
+
+
+//STEP 3
+
+//we will grab the unique property to fon d the user who has requested the page
+passport.deserializeUser((prop,done)=>{
+
+})
+
+
+}
+
+
+module.exports = Authentication;
