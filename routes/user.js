aba66f6e5112ec58fc8c7b8feb19606d7eede164
DEV270201 <devansh.js@somaiya.edu>
2021-06-14 11:27:30 +0530

    modified

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,45 @@
 const express = require("express");
 const userController = require("../controller/userController");
 const validateInput = require("../Joi/UserJoi");
+const passport = require("passport");
 
 const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.render("home" , {title : "Authenticator"});
+});
+
+router.get("/register",(req,res)=>{
+    res.render("register" , {title : "Registration Page"});
+})
+
+router.get("/login",(req,res)=>{
+    res.render("login" , {title : "Login Page"});
+});
 
 router.post("/register",async (req,res,next)=>{
  try { 
     const userCredentials = await validateInput({...req.body});
     await userController.Register(userCredentials);
-    res.status(201).json({
-        status : "success",
-        message : "User registered successfully",
-    });
+    res.redirect("/login");
+    // res.status(201).json({
+    //     status : "success",
+    //     message : "User registered successfully",
+    // });
 }catch(err){
    next(err);
 }
+});
+
+router.post("/login",passport.authenticate("local"),(req,res,next)=>{
+    try{
+        res.json({
+            Data : "hello",
+        })
+    }catch(err){
+        console.log("error occurred");
+        next(err);
+    }
 });
 
 module.exports = router;
